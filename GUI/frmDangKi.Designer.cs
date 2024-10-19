7723b32a2d7ef98e0d76c7472fae7bed7692332b
Hungi03 <hunghung2k33@gmail.com>
2024-10-19 09:10:12 +0700

    Resolved merge conflicts

---
+++ GUI/frmDangKi.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace GUI
+{
+    partial class frmDangKi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            panel2 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(558, 618);
+            panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(53, 551);
+            button2.Name = "button2";
+            button2.Size = new Size(155, 55);
+            button2.TabIndex = 6;
+            button2.Text = "Đăng kí";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.HotPink;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(320, 551);
+            button1.Name = "button1";
+            button1.Size = new Size(155, 55);
+            button1.TabIndex = 5;
+            button1.Text = "Đăng nhập";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 13.8F);
+            textBox2.Location = new Point(53, 415);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(422, 38);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 13.8F);
+            textBox1.Location = new Point(53, 317);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(422, 38);
+            textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F);
+            label3.Location = new Point(53, 369);
+            label3.Name = "label3";
+            label3.Size = new Size(110, 31);
+            label3.TabIndex = 2;
+            label3.Text = "Mật khẩu";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F);
+            label2.Location = new Point(53, 273);
+            label2.Name = "label2";
+            label2.Size = new Size(166, 31);
+            label2.TabIndex = 1;
+            label2.Text = "Tên đăng nhập";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DarkSeaGreen;
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(558, 257);
+            panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(159, 180);
+            label1.Name = "label1";
+            label1.Size = new Size(251, 38);
+            label1.TabIndex = 1;
+            label1.Text = "Đăng kí tài khoản";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.image_register;
+            pictureBox1.Location = new Point(203, 27);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(161, 135);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 13.8F);
+            textBox3.Location = new Point(53, 501);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(422, 38);
+            textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13.8F);
+            label4.Location = new Point(53, 456);
+            label4.Name = "label4";
+            label4.Size = new Size(201, 31);
+            label4.TabIndex = 7;
+            label4.Text = "Nhập lại mật khẩu";
+            // 
+            // frmDangKi
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(558, 618);
+            Controls.Add(panel1);
+            Name = "frmDangKi";
+            Text = "Đăng nhập";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox3;
+        private Label label4;
+    }
+}
---
+++ GUI/frmDonHang.cs
@@ -0,0 +1,383 @@
+﻿using Cassandra;
+using Cassandra.DataStax.Graph;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace GUI
+{
+    public partial class frmDonHang : Form
+    {
+        public Cluster cluster;
+        public ISession session;
+        public frmDonHang()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDonHang_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cluster = Cluster.Builder()
+                .AddContactPoint("127.0.0.1")
+                .Build();
+
+                session = cluster.Connect("qlgiaotan");
+
+
+                loadDSKhachHang();
+                loadDSDonHang();
+                data_binding();
+            }
+            catch
+            {
+                MessageBox.Show("Lỗi kết nối!");
+            }
+
+        }
+
+        private void data_binding()
+        {
+            cboKH.DataBindings.Clear();
+            cboKH.DataBindings.Add("TEXT", dgvDonHang.DataSource, "Tên Khách Hàng");
+
+            txtMaDH.DataBindings.Clear();
+            txtMaDH.DataBindings.Add("TEXT", dgvDonHang.DataSource, "Mã Đơn Hàng");
+
+            dtpNgayDat.DataBindings.Clear();
+            dtpNgayDat.DataBindings.Add("TEXT", dgvDonHang.DataSource, "Ngày Đặt");
+
+            txtTongTien.DataBindings.Clear();
+            txtTongTien.DataBindings.Add("TEXT", dgvDonHang.DataSource, "Tổng Tiền");
+
+            txtPTTT.DataBindings.Clear();
+            txtPTTT.DataBindings.Add("TEXT", dgvDonHang.DataSource, "PT Thanh Toán");
+
+            txtTrangThai.DataBindings.Clear();
+            txtTrangThai.DataBindings.Add("TEXT", dgvDonHang.DataSource, "Trạng Thái");
+        }
+
+        private void loadDSKhachHang()
+        {
+            // Tạo DataTable để chứa dữ liệu khách hàng
+            DataTable dtKhachHang = new DataTable();
+            dtKhachHang.Columns.Add("makh", typeof(string));
+            dtKhachHang.Columns.Add("tenkh", typeof(string));
+
+            var rs = session.Execute("SELECT * FROM khachhang");
+
+
+            foreach (var row in rs)
+            {
+                dtKhachHang.Rows.Add(row.GetValue<string>("makh"), row.GetValue<string>("tenkh"));
+            }
+
+            // Gán danh sách vào ComboBox
+            cboKH.DataSource = dtKhachHang;
+            cboKH.DisplayMember = "tenkh";  // Thuộc tính hiển thị là tên khách hàng
+            cboKH.ValueMember = "makh";
+        }
+
+        private void loadDSDonHang()
+        {
+            // Truy vấn để lấy danh sách sinh viên
+            var rs = session.Execute("SELECT madh, makh, ngaydat, tongtien, ptthanhtoan, trangthai FROM donhang");
+
+            // Tạo DataTable để chứa dữ liệu
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Mã Đơn Hàng", typeof(string));
+            dt.Columns.Add("Tên Khách Hàng", typeof(string));
+            dt.Columns.Add("Ngày Đặt", typeof(DateTime));
+            dt.Columns.Add("Tổng Tiền", typeof(string));
+            dt.Columns.Add("PT Thanh Toán", typeof(string));
+            dt.Columns.Add("Trạng Thái", typeof(string));
+
+            // Duyệt qua các hàng dữ liệu trả về
+            foreach (var row in rs)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Mã Đơn Hàng"] = row.GetValue<string>("madh");
+                dr["Tên Khách Hàng"] = layTenKHBangMaKH(row.GetValue<string>("makh"));
+                dr["Ngày Đặt"] = row.GetValue<DateTime>("ngaydat").ToShortDateString();
+                dr["Tổng Tiền"] = row.GetValue<double>("tongtien");
+                dr["PT Thanh Toán"] = row.GetValue<string>("ptthanhtoan");
+                dr["Trạng Thái"] = row.GetValue<string>("trangthai");
+                dt.Rows.Add(dr);
+            }
+
+            dgvDonHang.DataSource = dt;
+        }
+
+        private string layTenKHBangMaKH(string makh)
+        {
+            // Truy vấn tìm kiếm tên khách hàng dựa trên mã khách hàng
+            string query = "SELECT tenkh FROM khachhang WHERE makh = ?";
+            var preparedStatement = session.Prepare(query);
+            var boundStatement = preparedStatement.Bind(makh);
+
+            var rs = session.Execute(boundStatement);
+
+            // Lấy ra giá trị tên khách hàng từ kết quả truy vấn
+            var row = rs.FirstOrDefault();
+            if (row != null)
+            {
+                return row.GetValue<string>("tenkh");
+            }
+            else
+            {
+                return "null";
+            }
+        }
+        private string layMaKHBangTenKH(string tenkh)
+        {
+            string query = "SELECT makh FROM khachhang WHERE tenkh = ? ALLOW FILTERING";
+
+            var preparedStatement = session.Prepare(query);
+            var boundStatement = preparedStatement.Bind(tenkh);
+
+            var rs = session.Execute(boundStatement);
+
+            // Lấy kết quả từ hàng đầu tiên nếu có
+            var row = rs.FirstOrDefault();
+            if (row != null)
+            {
+                return row["makh"].ToString();
+            }
+            else
+            {
+                return "null";
+            }
+        }
+
+        private void btnThemDH_Click(object sender, EventArgs e)
+        {
+            txtMaDH.Clear();
+            dtpNgayDat.Value = DateTime.Now;
+            txtTongTien.Clear();
+            txtPTTT.Clear();
+            txtTrangThai.Clear();
+
+            txtMaDH.Focus();
+
+            btnLuu.Enabled = true;
+        }
+
+        private bool KTTrungMaDH(string madh)
+        {
+            bool isTrung = false;
+            try
+            {
+                string query = "SELECT count(*) FROM donhang WHERE madh = ? ALLOW FILTERING";
+
+                var preparedStatement = session.Prepare(query);
+                var boundStatement = preparedStatement.Bind(madh);
+
+                var rs = session.Execute(boundStatement);
+
+                // Lấy giá trị COUNT từ hàng đầu tiên của kết quả
+                int count = rs.First().GetValue<int>("count");
+
+                // Nếu tìm thấy mã đơn hàng, tức là mã đã tồn tại
+                if (count > 0)
+                {
+                    isTrung = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Kiểm tra trùng thất bại!");
+            }
+            return isTrung;
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (txtMaDH.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Vui lòng nhập Mã Đơn Hàng!");
+                txtMaDH.Focus();
+                return;
+            }
+
+            if (dtpNgayDat.Value > DateTime.Now)
+            {
+                MessageBox.Show("Vui lòng chọn Ngày Đặt hợp lệ!");
+                dtpNgayDat.Focus();
+                return;
+            }
+
+            if (txtTongTien.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Vui lòng nhập Tổng Tiền!");
+                txtTongTien.Focus();
+                return;
+            }
+
+            if (txtPTTT.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Vui lòng nhập PT Thanh Toán!");
+                txtPTTT.Focus();
+                return;
+            }
+
+            if (txtTrangThai.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Vui lòng nhập Trạng Thái!");
+                txtTrangThai.Focus();
+                return;
+            }
+
+            //if (KTTrungMaDH(txtMaDH.Text.Trim()))
+            //{
+            //    MessageBox.Show("Mã Đơn Hàng đã tồn tại!");
+            //    txtMaDH.Focus();
+            //    return;
+            //}
+
+            try
+            {               
+                string query = "INSERT INTO donhang (makh, madh, tongtien, ngaydat, ptthanhtoan, trangthai) VALUES (?, ?, ?, ?, ?, ?)";
+
+                var preparedStatement = session.Prepare(query);
+
+                string makh = cboKH.SelectedValue.ToString();
+                string madh = txtMaDH.Text;
+                double tongtien = double.Parse(txtTongTien.Text);
+                DateTime ngaydat = dtpNgayDat.Value;
+                string ptthanhtoan = txtPTTT.Text;
+                string trangthai = txtTrangThai.Text;
+
+                var boundStatement = preparedStatement.Bind(makh, madh, tongtien, ngaydat, ptthanhtoan, trangthai);
+
+                session.Execute(boundStatement);
+
+                MessageBox.Show("Thêm đơn hàng thành công!");
+                btnLuu.Enabled = false;
+                loadDSDonHang();
+                data_binding();
+            }
+            catch
+            {
+                MessageBox.Show("Thêm đơn hàng thất bại!");
+            }
+        }  
+        
+        private void btnXoaDH_Click(object sender, EventArgs e)
+        {
+            if (txtMaDH.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Vui lòng chọn đơn hàng cần xóa!");
+                return;
+            }
+
+            DialogResult r = MessageBox.Show("Bạn có chắc chắn muốn xóa đơn hàng " + txtMaDH.Text + " ?", "Xóa", MessageBoxButtons.YesNo,
+             MessageBoxIcon.Question);
+            if (r == DialogResult.No)
+                return;
+
+            try
+            {
+                string query = "DELETE FROM donhang WHERE madh = ? AND makh = ? AND tongtien = ?";
+
+                var preparedStatement = session.Prepare(query);
+
+                string madh = txtMaDH.Text;
+                string makh = cboKH.SelectedValue.ToString();
+                double tongtien = double.Parse(txtTongTien.Text);
+                var boundStatement = preparedStatement.Bind(madh, makh, tongtien);
+
+                session.Execute(boundStatement);
+
+                MessageBox.Show("Xóa đơn hàng thành công!");
+                loadDSDonHang();
+                data_binding();
+            }
+            catch
+            {
+                MessageBox.Show("Xóa đơn hàng thất bại!");
+            }
+        }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            loadDSDonHang();
+            data_binding();
+        }
+        string premadh, premakh;
+        double pretongtien;
+        private void btnSuaDH_Click(object sender, EventArgs e)
+        {
+            if (txtMaDH.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Vui lòng chọn đơn hàng cần sửa!");
+                return;
+            }
+
+            DialogResult r = MessageBox.Show("Bạn có chắc chắn muốn sửa đơn hàng " + txtMaDH.Text + " ?", "Xóa", MessageBoxButtons.YesNo,
+             MessageBoxIcon.Question);
+            if (r == DialogResult.No)
+                return;
+
+            try
+            {
+                //xóa đơn hàng cũ
+                string deleteQuery = "DELETE FROM donhang WHERE madh = ? AND makh = ? AND tongtien = ?";
+
+                var deletePreparedStatement = session.Prepare(deleteQuery);              
+                var deleteBoundStatement = deletePreparedStatement.Bind(premadh, premakh, pretongtien);
+
+                session.Execute(deleteBoundStatement);
+
+                //thêm đơn hàng mới để cập nhật các giá trị
+                string query = "INSERT INTO donhang (makh, madh, tongtien, ngaydat, ptthanhtoan, trangthai) VALUES (?, ?, ?, ?, ?, ?)";
+
+                var preparedStatement = session.Prepare(query);
+
+                string makh = cboKH.SelectedValue.ToString();
+                string madh = txtMaDH.Text;
+                double tongtien = double.Parse(txtTongTien.Text);
+                DateTime ngaydat = dtpNgayDat.Value;
+                string ptthanhtoan = txtPTTT.Text;
+                string trangthai = txtTrangThai.Text;
+
+                var boundStatement = preparedStatement.Bind(makh, madh, tongtien, ngaydat, ptthanhtoan, trangthai);
+
+                session.Execute(boundStatement);
+
+                MessageBox.Show("Đã cập nhật đơn hàng thành công!");
+                loadDSDonHang();
+                data_binding();
+            }
+            catch
+            {
+                MessageBox.Show("Cập nhật đơn hàng thất bại!");
+            }
+        }
+
+        private void dgvDonHang_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Lấy toàn bộ dòng hiện tại
+                DataGridViewRow row = dgvDonHang.Rows[e.RowIndex];
+
+                string tenkh = row.Cells["Tên Khách Hàng"].Value.ToString();
+                premadh = row.Cells["Mã Đơn Hàng"].Value.ToString();
+                premakh = layMaKHBangTenKH(tenkh);
+                pretongtien = Convert.ToDouble(row.Cells["Tổng Tiền"].Value);
+
+                //DateTime ngayDat = Convert.ToDateTime(row.Cells["Ngày Đặt"].Value);                   
+                //string trangThai = row.Cells["Trạng Thái"].Value.ToString();  
+                //string ptThanhToan = row.Cells["PT Thanh Toán"].Value.ToString();  
+            }
+        }
+    }
+}
